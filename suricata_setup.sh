eba5d6ee99cef2e6c7fdf0e79401f1d9a99bee47
Starke427 <jstarke@superuser.life>
2019-07-28 02:14:17 -0700

    Update suricata_setup.sh

--- suricata_setup.sh
+++ suricata_setup.sh
@@ -17,7 +17,7 @@ suricata -T
 # Configure Suricata listener to restart on reboot with crontab and update at 0200 UTC on Tuesdays
 crontab -l > mycron
 echo "@reboot sudo suricata -c /etc/suricata/suricata.yaml -i ${1-$(ifconfig | grep -m1 flag | cut -d":" -f1)} -D" >> mycron
-echo "* 2 * * 2 sudo suricata-update" >> mycron
+echo "* 2 * * * sudo suricata-update" >> mycron
 crontab mycron
 rm -f mycron
 
