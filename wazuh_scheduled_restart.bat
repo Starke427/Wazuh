abb98665412ba0224570353a3153c8d19f49e899
Starke427 <jstarke@superuser.life>
2020-03-12 11:48:35 -0700

    Update wazuh_scheduled_restart.bat

--- wazuh_scheduled_restart.bat
+++ wazuh_scheduled_restart.bat
@@ -4,10 +4,14 @@ REM Author: Jeff Starke
 
 REM Create service restart script
 (
-echo net stop wazuh 2> nul
+echo net stop wazuh  2>&1>nul
 echo net start wazuh
 )>"C:\wazuh_restart.bat"
 
 REM Create scheduled task to run script
-SCHTASKS /DELETE /TN "Microsoft\Windows\Wazuh\Restart Wazuh" /F 2> nul
+SCHTASKS /DELETE /TN "Microsoft\Windows\Wazuh\Restart Wazuh" /F  2>&1>nul
 SCHTASKS /CREATE /SC DAILY /NP /TN "Microsoft\Windows\Wazuh\Restart Wazuh" /TR "C:wazuh_restart.sh" /ST 23:00
+
+REM Restart service now
+echo net stop wazuh  2>&1>nul
+echo net start wazuh
