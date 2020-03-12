11734312bea5c4a29d9ebdef2130d6925eacbf26
Starke427 <jstarke@superuser.life>
2020-03-12 11:51:49 -0700

    Update wazuh_scheduled_restart.bat

--- wazuh_scheduled_restart.bat
+++ wazuh_scheduled_restart.bat
@@ -1,17 +1,13 @@
-REM Basic batch script for creating a local script and local scheduled task for running aforementioned script. 2>&1>nul
-REM Author: Jeff Starke 2>&1>nul
+REM Basic batch script for creating a local script and local scheduled task for running aforementioned script.
+REM Author: Jeff Starke
 
-
-REM Create service restart script 2>&1>nul
 (
-echo net stop wazuh  2>&1>nul
+echo net stop wazuh
 echo net start wazuh
 )>"C:\wazuh_restart.bat"
 
-REM Create scheduled task to run script 2>&1>nul
-SCHTASKS /DELETE /TN "Microsoft\Windows\Wazuh\Restart Wazuh" /F  2>&1>nul
+SCHTASKS /DELETE /TN "Microsoft\Windows\Wazuh\Restart Wazuh" /F
 SCHTASKS /CREATE /SC DAILY /NP /TN "Microsoft\Windows\Wazuh\Restart Wazuh" /TR "C:wazuh_restart.sh" /ST 23:00
 
-REM Restart service now 2>&1>nul
-echo net stop wazuh  2>&1>nul
+echo net stop wazuh
 echo net start wazuh
